365cb20fe657331fb1e876ceaab9b247a3409455
mahad852 <mahad852@gmail.com>
2024-11-29 12:58:59 -0500

    added rlaif-v to names

--- llava/mm_utils.py
+++ llava/mm_utils.py
@@ -211,7 +211,7 @@ def get_model_name_from_path(model_path):
     for mp in model_paths:
         if "llava" in mp.lower():
             return f"{mp.lower()}"
-        if "povid" in mp.lower() or "autohal" in mp.lower():
+        if "povid" in mp.lower() or "autohal" in mp.lower() or "rlaif-v" in mp.lower():
             return f"{mp}_llava_lora"
     
     if model_paths[-1].startswith('checkpoint-'):
