294de943beb8ffec2ca2273dad0f754fe0e3c108
mahad852 <mahad852@gmail.com>
2024-11-30 12:01:37 -0500

    changed output path in run_dpo.sh

--- scripts/run_dpo.sh
+++ scripts/run_dpo.sh
@@ -12,7 +12,7 @@ python llava/train/train_dpo.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 False \
-    --output_dir ./checkpoint/output/POVID_stage_one_LoRa \
+    --output_dir ./checkpoint/output/POVID_stage_one_og \
     --num_train_epochs 3 \
     --per_device_train_batch_size 1\
     --per_device_eval_batch_size 1 \
